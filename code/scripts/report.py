b7fa6bff7b29284ce4214954f70307b379ffe0a6
albert-kevin <beire_@hotmail.com>
2023-05-19 11:30:11 +0200

    add backup of input data into zip file and root folder

--- code/scripts/report.py
+++ code/scripts/report.py
@@ -10,19 +10,21 @@ df = pd.read_csv(csv_file, sep=';', encoding='utf-8')
 df = df.convert_dtypes()
 # make the names snake_case (remove spaces)
 df['Persoon'] = df['Persoon'].apply(lambda x: x.replace(' ', '_'))
-# backup copy
-df.to_csv("../../data/bronze/input_backup_" + datetime.now().strftime("%d%b%Y"), index=False, sep=';', encoding='utf-8')
 
-# Create a zip file to store the Excel files
+# Create backup copy
+backup_date = datetime.now().strftime("%d%b%Y")
+backup_file = "../../data/bronze/input_backup_" + backup_date + ".csv"
+df.to_csv(backup_file, index=False, sep=';', encoding='utf-8')
+
+# Create a zip file to store the Excel files and the backup csv
 path = f"../../data/silver/"
 zip_filename = f"{path}{datetime.now().strftime('%d%b%Y')}_Vluchtlogboeken.zip"
-# Group data by person
 grouped = df.groupby("Persoon")
 with zipfile.ZipFile(zip_filename, 'w') as zip_file:
 
     # Loop over each person and create a separate Excel file for their data
     for name, group in grouped:
-        xslx_filename = f"{path}{datetime.now().strftime('%d%b%Y')}__{name}__Vluchtlogboek.xlsx"
+        xslx_filename = f"{path}{name}__{datetime.now().strftime('%d%b%Y')}__Vluchtlogboek.xlsx"
         # Remove the "Persoon" column from the DataFrame
         group = group.drop(columns=["Persoon"])
         # Write the person's data to a sheet in the Excel file
@@ -34,4 +36,7 @@ with zipfile.ZipFile(zip_filename, 'w') as zip_file:
                 column_width = max(group[col].astype(str).map(len).max(), len(col)) + 2
                 worksheet.set_column(i, i, column_width)
         # Add the Excel file to the zip file
-        zip_file.write(xslx_filename)
+        zip_file.write(xslx_filename, arcname=os.path.basename(xslx_filename))
+
+    # Add the backup CSV to the zip file
+    zip_file.write(backup_file, arcname=os.path.basename(backup_file))
